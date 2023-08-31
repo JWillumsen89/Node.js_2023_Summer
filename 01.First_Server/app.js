dc89b7112b2ea6fb30ab79cdec8bc986ada178e1
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2023-08-31 11:22:44 +0200

    added mountains rest api and design

--- 01.First_Server/app.js
+++ 01.First_Server/app.js
@@ -3,11 +3,43 @@
 //Install Express
 //const app = express();
 
-//Same as above.
+//Same as above. But use the above instead in the future.
 const app = require('express')();
 
-app.get("/", (req,res) => {
-  res.send({data: "This is the first request handler"});
+app.get('/', (req, res) => {
+    res.send({ data: 'This is the first request handler' });
+});
+
+// task: create a dog endpoint that returns woof
+
+app.get('/dog', (req, res) => {
+    console.log(req.params);
+    res.send({ bark: 'Woof' });
+});
+
+app.get('/dog/:firstValue/:id', (req, res) => {
+    console.log(req.params.id);
+    console.log(req.params);
+    res.send({ dog: 'Meow' });
+});
+
+let balance = 100;
+
+app.get('/wallet/:withdrawalAmount', (req, res) => {
+    const withdrawalAmount = req.params.withdrawalAmount;
+    balance -= withdrawalAmount;
+    if (balance < 0) {
+        res.send({ message: 'You cant withdraw, No money left.' });
+    }
+    if (req.params.withdrawalAmount > balance) {
+        res.send({
+            message: `You cant withdraw ${req.params.withdrawalAmount} dkr - Your current balance is: ${balance} dkr`,
+        });
+    } else {
+        res.send({
+            message: `You've withdrawn ${req.params.withdrawalAmount} dkr - Your current balance is ${balance - req.params.withdrawalAmount} dkr`,
+        });
+    }
 });
 
 //80 http
