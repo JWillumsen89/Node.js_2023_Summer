19ac09bc8e6fe06a279a572be09df351fe592850
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2023-09-12 11:46:17 +0200

    added assignments

--- 03._First_Html_Server/app.js
+++ 03._First_Html_Server/app.js
@@ -9,6 +9,15 @@ app.get('/secondPage', (req, res) => {
     res.sendFile(__dirname + '/public/secondPage.html');
 });
 
+app.get('/welcomemessage', (req, res) => {
+    const clientName = req.query.user;
+    if (!clientName) {
+        res.send({ data: `Hello there, stranger.` });
+    } else {
+        res.send({ data: `Hello there, ${req.query.user}.` });
+    }
+});
+
 const PORT = 8080;
 
 app.listen(PORT, error => {
