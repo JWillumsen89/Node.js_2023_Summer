66b5104a31a43e813a6e6f4719461f71c77fba43
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2023-09-24 12:01:22 +0200

    testing

--- Mandatory_I/app.js
+++ Mandatory_I/app.js
@@ -5,24 +5,12 @@ app.use(express.static('public'));
 
 import path from 'path';
 
-app.get('/', (req, res, next) => {
-    try {
-        const filePath = path.resolve('./public/html/frontpage.html');
-        res.sendFile(filePath);
-    } catch (error) {
-        console.error('Error sending the file:', error);
-        next(error); // Pass the error to the error handling middleware
-    }
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./public/html/frontpage.html'));
 });
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT);
-});
-
-// Add error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
 });
