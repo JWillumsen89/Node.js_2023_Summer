44a46745e43b8eae361581a2a0f7cc207b207bf2
JWillumsen89 <100369030+JWillumsen89@users.noreply.github.com>
2023-10-02 12:34:53 +0200

    added admin page

--- Mandatory_I/app.js
+++ Mandatory_I/app.js
@@ -1,9 +1,32 @@
 import express from 'express';
 const app = express();
+import session from 'express-session';
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: 'f4a82d3d88a549f3be2de0038b8d9db8d6c6f7e650784e72c4812f7daea983ef',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false },
+    })
+);
 
 import path from 'path';
+
+const users = [
+    {
+        username: 'test',
+        password: 'password123',
+    },
+    {
+        username: 'test2',
+        password: 'password456',
+    },
+];
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./public/homepage/homepage.html'));
@@ -32,6 +55,50 @@ app.get('/login', (req, res) => {
 app.get('/contact', (req, res) => {
     res.sendFile(path.resolve('./public/contactpage/contactpage.html'));
 });
+
+app.get('/adminpage', (req, res) => {
+    if (req.session.isAuthenticated) {
+        res.sendFile(path.resolve('./public/adminpage/adminpage.html'));
+    } else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect('/adminpage');
+        }
+
+        res.clearCookie('connect.sid');
+        return res.redirect('/login');
+    });
+});
+
+app.get('/auth_status', (req, res) => {
+    if (req.session.isAuthenticated) {
+        res.json({ isAuthenticated: true });
+    } else {
+        res.json({ isAuthenticated: false });
+    }
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const user = users.find(user => user.username === username);
+    if (user) {
+        if (user.password === password) {
+            req.session.isAuthenticated = true;
+            res.redirect('/adminpage');
+        } else {
+            res.status(401).send({ success: false, message: 'Incorrect password.' });
+        }
+    } else {
+        res.status(404).send({ success: false, message: 'User not found.' });
+    }
+});
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
--- Mandatory_I/public/assets/js/navbar.js
+++ Mandatory_I/public/assets/js/navbar.js
@@ -1,22 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navbarHTML = `
-        <navbar id="os-navbar">
-            <div class="navbar-header">My Website</div>
-            <div class="navbar-container">
-                <a href="/" class="nav-item">Home</a>
-                <div class="nav-item-dropdown">
-                    <span class="nav-item">Subjects</span>
-                    <div class="dropdown-content">
-                        <a href="/node.js">Node.js</a>
-                        <a href="/express.js">Express.js</a>
-                        <a href="/javascriptfundamentals">Javascript Fundamentals</a>
-                    </div>
-                </div>
-                <a href="/contact" class="nav-item">Contact</a>
-                <a href="/login" class="nav-item">Login</a>
-            </div>
-        </navbar>
-    `;
+    const setNavbar = authStatus => {
+        let authButton = authStatus ? '<a href="/logout" class="nav-item" id="logout">Logout</a>' : '<a href="/login" class="nav-item">Login</a>';
 
-    document.body.prepend(new DOMParser().parseFromString(navbarHTML, 'text/html').body.firstChild);
+        const navbarHTML = `
+            <navbar id="os-navbar">
+                <div class="navbar-header">My Website</div>
+                <div class="navbar-container">
+                    <a href="/" class="nav-item">Home</a>
+                    <div class="nav-item-dropdown">
+                        <span class="nav-item">Subjects</span>
+                        <div class="dropdown-content">
+                            <a href="/node.js">Node.js</a>
+                            <a href="/express.js">Express.js</a>
+                            <a href="/javascriptfundamentals">Javascript Fundamentals</a>
+                        </div>
+                    </div>
+                    <a href="/contact" class="nav-item">Contact</a>
+                    ${authButton}
+                </div>
+            </navbar>
+        `;
+        document.body.prepend(new DOMParser().parseFromString(navbarHTML, 'text/html').body.firstChild);
+    };
+
+    fetch('/auth_status', {
+        method: 'GET',
+        credentials: 'same-origin',
+    })
+        .then(response => response.json())
+        .then(data => {
+            setNavbar(data.isAuthenticated);
+        });
 });
